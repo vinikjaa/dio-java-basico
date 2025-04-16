4cb0ad4a28da38a47ac2c246491c6df5dcc02d5d
Vinikjaa <vini.jesusabreu@gmail.com>
2025-04-16 10:53:23 -0300

    meu primeiro projeto java

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Salve, este é meu primeiro comando para a comunidade, Turma DIO");
+    }
+}
